c7fd278815653c9712eba0f06aa4a3b53048169b
Subhankar Samanta <100989212+passion-lab@users.noreply.github.com>
2023-01-01 23:51:44 +0530

    Backend file added

---
+++ AppBackend.py
@@ -0,0 +1,11 @@
+# Gets a directory or path and find music files (e.g., *.mp3, *.wav, *.aac) for processing
+
+from tkinter import Frame
+from os import curdir, listdir
+from os.path import isdir, isfile, exists
+
+
+class Filesystem:
+
+    def __init__(self, ):
+        pass
