56bbab0aebc97cc9dc0de3f1d5cb2f21896e2aba
Subhankar Samanta <100989212+passion-lab@users.noreply.github.com>
2023-01-03 10:25:17 +0530

    Many default values are added
    Several default values are added for many keys.

--- AppDefaults.py
+++ AppDefaults.py
@@ -11,10 +11,14 @@ class ColorDefaults:
         match self.theme:
             case "white":
                 self.main_back = "white"
-                self.head_back = "#D7D7D7"
-
                 self.main_fore = "black"
+                self.head_back = "#e6e6e6"
                 self.head_fore = "#1F1F1F"
+                self.entry_back = "#dbdbdb"
+                self.entry_back_hover = "#cacaca"
+                self.entry_title_fore = "black"
+                self.entry_key_fore = "#919191"
+                self.entry_value_fore = "#474747"
 
 
 class FontDefaults:
@@ -22,6 +26,8 @@ class FontDefaults:
     def __init__(self):
         self.title = ()
         self.subtitle = ()
-        self.heading = ('Roboto Condensed Bold', 18)
         self.menu = ('Roboto Condensed', 11)
         self.body = ()
+        self.heading = ('Roboto Condensed Light', 11)
+        self.key = ('Roboto Condensed', 9)
+        self.value = ('Roboto Condensed Light', 9)
